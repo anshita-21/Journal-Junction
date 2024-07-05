f032bb0e3f486569a96752ad7f950d61d5ba6973
anshita-21 <140099324+anshita-21@users.noreply.github.com>
2024-07-05 23:21:56 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         //target: 'http://localhost:3000',
-        target: 'https://journal-junction-1.onrender.com',
+        target: 'https://journal-junction-b.onrender.com',
         changeOrigin: true,
       },
     },
