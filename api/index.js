18397451ac12d87a49a601d161ad9b7e02c674e7
anshita-21 <140099324+anshita-21@users.noreply.github.com>
2024-07-06 12:23:25 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,38 +1,42 @@
 import express from "express";
-import AuthRoutes from "./routes/authRoutes.js"
+import AuthRoutes from "./routes/authRoutes.js";
 import mongoose from "mongoose";
-import UserRouter from './routes/userRoutes.js'
+import UserRouter from './routes/userRoutes.js';
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
 
-
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(process.env.MONGO).then(() => {
     console.log("DB connected");
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
-})
+});
 
 const app = express();
-app.use(express.json());                                    
-app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use('/api/auth',AuthRoutes);
-app.use('/api/user',UserRouter);
 
+// Middleware to disable caching
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.set('Pragma', 'no-cache');
+    res.set('Expires', '0');
+    next();
+});
 
+app.use('/api/auth', AuthRoutes);
+app.use('/api/user', UserRouter);
 
 app.listen(3000, () => console.log("Server started on port 3000"));
 
-
-
-app.use((err,req,res,next)=>{
+// Error handling middleware
+app.use((err, req, res, next) => {
     const status = err.status || 500;
     const message = err.message || "Something went wrong";
     return res.status(status).json({
-        success : false,
+        success: false,
         status,
         message
     });
-    
-})
+});
